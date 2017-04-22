7e9229f2cf46d28cb93f1da4ed75b9088e5dda53
settenke <settenke@gmail.com>
2017-04-22 14:00:31 +0200

    feat(chen): fix chen

--- components/chenHelper.js
+++ components/chenHelper.js
@@ -1,10 +1,26 @@
 'use strict';
 
+const cardlist = {
+  '2': 0,
+  '3': 1,
+  '4': 2,
+  '5': 3,
+  '6': 4,
+  '7': 5,
+  '8': 6,
+  '9': 7,
+  '10': 8,
+  'J': 9,
+  'Q': 10,
+  'K': 11,
+  'A': 12
+};
+
 class ChenHelper {
 
   constructor(hand){
-    // console.log('hand', hand);
-    // console.log();
+    console.log('hand', hand);
+    console.log();
     this.firstRank = hand[0]["rank"];
     this.secondRank = hand[1]["rank"];
     this.firstSuit = hand[0]["suit"];
@@ -15,29 +31,42 @@ class ChenHelper {
           7 : this.firstRank == "J" ?
             6 : parseInt(this.firstRank)/2;
 
-    // console.log('first:', this.firstValue);
+    console.log('first:', this.firstValue);
     this.secondValue = this.secondRank == "A" ?
       10 : this.secondRank == "K" ?
         8 : this.secondRank == "Q" ?
           7 : this.secondRank == "J" ?
             6 : parseInt(this.secondRank)/2;
 
-    // console.log('sec:', this.secondValue);
-    // console.log();
+    console.log('sec:', this.secondValue);
+    console.log();
   }
 
   calculatePoints(){
     let betAmount = this.getScoreForHighest();
-    // console.log('based max', betAmount);
+    console.log('based max', betAmount);
     betAmount = this.firstRank == this.secondRank ? betAmount * 2 : betAmount;
     betAmount = this.firstSuit == this.secondSuit ? betAmount + 2 : betAmount;
-    let gap = Math.abs(this.firstValue - this.secondValue);
+    let gapFirst = cardlist[this.firstRank];
+    let gapSec = cardlist[this.secondRank];
+    let gap;
+    if (gapFirst == gapSec) {
+      gap = 0;
+    } else {
+      gap = Math.abs(gapFirst - gapSec) - 1;
+    }
     betAmount = gap == 0 ?
       betAmount : gap == 1 ?
       betAmount - 1 : gap == 2 ?
       betAmount - 2 : gap == 3 ?
       betAmount - 4 : betAmount - 5;
-    betAmount = gap < 2 && this.firstValue < 7 && this.secondValue < 7 ? betAmount + 1 : betAmount;
+
+    console.log('BET', betAmount);
+    console.log('gap', gap);
+    console.log('this.firstValue', this.firstValue);
+    console.log('this.secondValue', this.secondValue);
+    betAmount = gap < 2 && this.firstValue < 7 && this.secondValue < 7 && this.firstValue !== this.secondValue ? betAmount + 1 : betAmount;
+    console.log('BETU', betAmount);
     return Math.ceil(betAmount)
   }
 
@@ -47,7 +76,7 @@ class ChenHelper {
 
   calculate(){
     let points = this.calculatePoints()
-    // console.log(points);
+    console.log(points);
     return points < 9 ? 0 : 1000;
   }
 
--- test/test.js
+++ test/test.js
@@ -26,14 +26,14 @@ describe('LeanPoker', function() {
       }
     });
 
-    it('should 66 bet: 1000', function() {
+    it('should 66 bet: 0', function() {
       GameState.players[1].hole_cards[0].rank = '6';
       GameState.players[1].hole_cards[1].rank = '6';
       GameState.players[1].hole_cards[0].suit = 'hearts';
       GameState.players[1].hole_cards[1].suit = 'spades';
       let bet = sinon.spy();
       Player.betRequest(GameState, bet);
-      bet.should.have.been.calledWith(1000);
+      bet.should.have.been.calledWith(0);
     });
 
     it('should AJ bet: 1000', function() {
@@ -43,10 +43,10 @@ describe('LeanPoker', function() {
       GameState.players[1].hole_cards[1].suit = 'spades';
       let bet = sinon.spy();
       Player.betRequest(GameState, bet);
-      bet.should.have.been.calledWith(1000);
+      bet.should.have.been.calledWith(0);
     });
 
-    it.only('should K7 bet: 0', function() {
+    it('should K7 bet: 0', function() {
       GameState.players[1].hole_cards[0].rank = 'K';
       GameState.players[1].hole_cards[1].rank = '7';
       GameState.players[1].hole_cards[0].suit = 'hearts';
@@ -59,7 +59,7 @@ describe('LeanPoker', function() {
   });
 
   describe('chenHelper', function() {
-    it('should xx', function() {
+    it('should K7 suited calc chen score: 5', function() {
       GameState.players[1].hole_cards[0].rank = 'K';
       GameState.players[1].hole_cards[1].rank = '7';
       GameState.players[1].hole_cards[0].suit = 'hearts';
@@ -68,7 +68,74 @@ describe('LeanPoker', function() {
       let ourPlayer = data["players"].filter((player) => player.name == "happyDay")[0];
 
       let helper = new ChenHelper(ourPlayer["hole_cards"]);
-      let betAmount = helper.calculate();
+      let betAmount = helper.calculatePoints();
+      expect(betAmount).to.eql(5);
+    });
+
+    it('should AK suited calc chen score: 12', function() {
+      GameState.players[1].hole_cards[0].rank = 'A';
+      GameState.players[1].hole_cards[1].rank = 'K';
+      GameState.players[1].hole_cards[0].suit = 'hearts';
+      GameState.players[1].hole_cards[1].suit = 'hearts';
+      let data = GameState;
+      let ourPlayer = data["players"].filter((player) => player.name == "happyDay")[0];
+
+      let helper = new ChenHelper(ourPlayer["hole_cards"]);
+      let betAmount = helper.calculatePoints();
+      expect(betAmount).to.eql(12);
+    });
+
+    it('should TT calc chen score: 10', function() {
+      GameState.players[1].hole_cards[0].rank = '10';
+      GameState.players[1].hole_cards[1].rank = '10';
+      GameState.players[1].hole_cards[0].suit = 'spades';
+      GameState.players[1].hole_cards[1].suit = 'hearts';
+      let data = GameState;
+      let ourPlayer = data["players"].filter((player) => player.name == "happyDay")[0];
+
+      let helper = new ChenHelper(ourPlayer["hole_cards"]);
+      let betAmount = helper.calculatePoints();
+      expect(betAmount).to.eql(10);
+    });
+
+    it('should 5 7 suited calc chen score: 6', function() {
+      GameState.players[1].hole_cards[0].rank = '5';
+      GameState.players[1].hole_cards[1].rank = '7';
+      GameState.players[1].hole_cards[0].suit = 'hearts';
+      GameState.players[1].hole_cards[1].suit = 'hearts';
+      let data = GameState;
+      let ourPlayer = data["players"].filter((player) => player.name == "happyDay")[0];
+
+      let helper = new ChenHelper(ourPlayer["hole_cards"]);
+      let betAmount = helper.calculatePoints();
+      expect(betAmount).to.eql(6);
+    });
+
+    it('should 2 7 calc chen score: -1', function() {
+      GameState.players[1].hole_cards[0].rank = '2';
+      GameState.players[1].hole_cards[1].rank = '7';
+      GameState.players[1].hole_cards[0].suit = 'spades';
+      GameState.players[1].hole_cards[1].suit = 'hearts';
+      let data = GameState;
+      let ourPlayer = data["players"].filter((player) => player.name == "happyDay")[0];
+
+      let helper = new ChenHelper(ourPlayer["hole_cards"]);
+      let betAmount = helper.calculatePoints();
+      expect(betAmount).to.eql(-1);
+    });
+
+
+    it('should AA calc chen score: -1', function() {
+      GameState.players[1].hole_cards[0].rank = 'A';
+      GameState.players[1].hole_cards[1].rank = 'A';
+      GameState.players[1].hole_cards[0].suit = 'spades';
+      GameState.players[1].hole_cards[1].suit = 'hearts';
+      let data = GameState;
+      let ourPlayer = data["players"].filter((player) => player.name == "happyDay")[0];
+
+      let helper = new ChenHelper(ourPlayer["hole_cards"]);
+      let betAmount = helper.calculatePoints();
+      expect(betAmount).to.eql(20);
     });
 
   });
