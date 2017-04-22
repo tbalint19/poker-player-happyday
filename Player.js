3e447ec055e17433bf61b60a6970949dee899320
tbalint19 <toth910719balint@gmail.com>
2017-04-22 10:26:14 +0200

    second

--- Player.js
+++ Player.js
@@ -4,7 +4,19 @@ class Player {
   }
 
   static betRequest(gameState, bet) {
-    bet(1000);
+    try {
+      let data = JSON.parse(gameState);
+      let ourPlayer = data["players"].filter((player) => player.name == "happyDay"));
+      let ourHand = ourPlayer["hole_cards"];
+      let firstRank = ourHand[0]["rank"];
+      let secondRank = ourHand[1]["rank"];
+      let highRanks = ["A", "K", "Q", "J", "10"];
+      let betAmount = firstRank == secondRank ? 1000 : highRanks.includes(firstRank) && highRanks.includes(secondRank) ? 1000 : 0;
+      bet(betAmount);
+    } catch (e){
+      console.log("error happened", e);
+      bet(1000);
+    }
   }
 
   static showdown(gameState) {
