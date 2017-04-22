1695f1e773e8fb74280309e319c0ec554f3abf65
tbalint19 <toth910719balint@gmail.com>
2017-04-22 10:39:25 +0200

    secondAgain

--- Player.js
+++ Player.js
@@ -5,13 +5,13 @@ class Player {
 
   static betRequest(gameState, bet) {
     try {
-      // let data = JSON.parse(gameState);
-      // let ourPlayer = data["players"].filter((player) => player.name == "happyDay"));
-      // let ourHand = ourPlayer["hole_cards"];
-      // let firstRank = ourHand[0]["rank"];
-      // let secondRank = ourHand[1]["rank"];
-      // let highRanks = ["A", "K", "Q", "J", "10"];
-      // let betAmount = firstRank == secondRank ? 1000 : highRanks.includes(firstRank) && highRanks.includes(secondRank) ? 1000 : 0;
+      let data = JSON.parse(gameState);
+      let ourPlayer = data["players"].filter((player) => player.name == "happyDay");
+      let ourHand = ourPlayer["hole_cards"];
+      let firstRank = ourHand[0]["rank"];
+      let secondRank = ourHand[1]["rank"];
+      let highRanks = ["A", "K", "Q", "J", "10"];
+      let betAmount = firstRank == secondRank ? 1000 : highRanks.includes(firstRank) && highRanks.includes(secondRank) ? 1000 : 0;
       bet(betAmount);
     } catch (e){
       console.log("error happened", e);
