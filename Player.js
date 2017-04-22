3c69105946301e65bee0a3cf77ed047c4888a918
tbalint19 <toth910719balint@gmail.com>
2017-04-22 14:15:00 +0200

    chenx

--- Player.js
+++ Player.js
@@ -14,10 +14,14 @@ class Player {
     let ourPlayer = data["players"].filter((player) => player.name == "happyDay")[0];
     let helper;
     let betAmount;
+    let agressiveTreshold = 7;
+    let passiveTreshold = 9;
+    let numberOfPlayers = data["players"].filter((player) => player.status == "active" || player.status == "folded").length;
+    let currentTreshold = numberOfPlayers > 3 ? passiveTreshold : agressiveTreshold;
     try {
       helper = new ChenHelper(ourPlayer["hole_cards"]);
       let points = helper.calculatePoints();
-      betAmount = points < 7 ? 0 : 5000;
+      betAmount = points < currentTreshold ? 0 : 5000;
       bet(betAmount);
     } catch (e){
       helper = new BasicHelper(ourPlayer["hole_cards"]);
